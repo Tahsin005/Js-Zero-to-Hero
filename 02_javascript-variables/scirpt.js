406607f1a0c11be7bab0eec2ed85b62699b0cc2b
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-20 23:17:40 +0600

    javascript variables in depth

---
+++ 02_javascript-variables/scirpt.js
@@ -0,0 +1,9 @@
+let userName = 'Tahsin';
+let age = 22
+
+let isHappy = true 
+let userIntro = 'Hi i am ' + userName + '. I am ' + age + ' years old.'
+
+let a = null // type is 'object'
+
+const hoursInDay = 24
