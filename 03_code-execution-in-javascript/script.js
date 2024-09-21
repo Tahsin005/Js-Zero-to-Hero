e8964497d38497346999bf4827b700a57df109e2
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-21 18:26:01 +0600

    executing code line by line in devtools

---
+++ 03_code-execution-in-javascript/script.js
@@ -0,0 +1,9 @@
+// debugger
+console.log(firstName)  
+
+var firstName = 'Tahsin'
+let lastName = 'Ferdous'
+let age = 15
+const yearOfBirth = 1999
+
+let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
