c51e09a161d876001363f6a2922161c16ce1a252
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-22 21:18:00 +0600

    math object in js

---
+++ 06_maths-in-javascript/scripts.js
@@ -0,0 +1,39 @@
+console.log(5 ** 2 );
+
+console.log(Math.PI);
+
+console.log(Math.SQRT2);
+
+console.log(Math.sqrt(16));
+
+console.log(Math.pow(4, 2));
+
+console.log(Math.floor(10 / 3));
+
+console.log(Math.floor(-2.5));
+
+console.log(Math.ceil(10 / 3)); 
+
+console.log(Math.round(2.3));
+
+console.log(Math.round(2.7));
+
+console.log(Math.round(-2.3));
+
+console.log(Math.round(-2.7));
+
+console.log(Math.round(Math.random() * 20));
+
+console.log((Math.random() * (20 - 10 + 1)) + 10);
+
+console.log(8 / 0);
+
+console.log(-8 / 0);
+
+console.log(0 / 0);
+
+const width = +prompt("Please Enter Rectangle Width");
+const height = +prompt("Please Enter Rectangle Height");
+
+console.log(width * height);
+document.write(`Rectangle Area: ${width * height}`);
