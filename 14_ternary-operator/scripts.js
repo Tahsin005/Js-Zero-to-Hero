dc0a2ce0f6e8f6e206f35a40e75dc38a8739ffc9
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-26 10:18:57 +0600

    Ternary Operator

---
+++ 14_ternary-operator/scripts.js
@@ -0,0 +1,8 @@
+const username = 5 > 2 ? 'Tahsin' : 'Ferdous'
+
+const gender = 'F'
+const userMessage = `${gender.toLowerCase() === 'f' ? 'She': 'He'} is a college student.`
+
+const result = null ? 'Tahsin' : '' ? '12' : 0
+
+console.log(result)
