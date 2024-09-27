d27773239a164602320cc1b47f0408a9ce8c0b5f
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-27 18:22:37 +0600

    array in js

---
+++ 18_array-in-js/scripts.js
@@ -0,0 +1,11 @@
+
+const fruitsCollection = ['Apple', 'Banana', 'Grapes', 'Dates']
+
+const newObject = {}
+const newArray = []
+
+
+newArray.firstName = 'Tahsin'
+
+console.log(newArray.firstName);
+console.log(newArray.length);
