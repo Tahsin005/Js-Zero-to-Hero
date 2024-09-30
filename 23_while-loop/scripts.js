ad55d6b3cd15babeceffb65563d6b4e0bbd91ebc
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-30 18:33:17 +0600

    while loop in js

---
+++ 23_while-loop/scripts.js
@@ -0,0 +1,28 @@
+console.log('Program starting...');
+
+let num = 0;
+
+while (num < 10) {
+    console.log(num);
+    num++;
+}
+
+const friends = ['Tahsin', 'Niloy', 'Mahin', 'Aziz', 'Ridwan', 'Rean', 'Ferdous']
+
+let i = 0
+
+while(i < friends.length) {
+    console.log(`${i + 1}. ${friends[i]}`);
+    friends[i] = friends[i] + ' Procodrr'
+    i++
+}
+
+i = 0
+
+while(i < friends.length) {
+    console.log(`${i + 1}. ${friends[i]}`);
+    friends[i] = friends[i] + ' Procodrr'
+    i++
+}
+
+console.log('Program Ended')
