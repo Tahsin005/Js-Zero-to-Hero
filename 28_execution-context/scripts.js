466fe3183426295243199e51ca56e2257a02ec08
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-10-02 16:51:39 +0600

    execution context in js

---
+++ 28_execution-context/scripts.js
@@ -0,0 +1,23 @@
+
+
+sayHi();
+
+const username = 'Tahsin';
+const userAge = 22;
+
+function sayHi() {
+    const a = 14;
+    const b = 12;
+    add(7, 9);
+}
+
+function add(x, y) {
+    kuchhBhi();
+    return x + y;
+}
+
+function kuchhBhi() {
+    console.log('Kuchh bhi');
+}
+
+console.log('Program Ended');
