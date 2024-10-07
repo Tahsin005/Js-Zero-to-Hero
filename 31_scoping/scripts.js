8e84a30d7e961db02982c9fd75b692ab167b9907
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-10-07 11:21:50 +0600

    global scope vs local scope

---
+++ 31_scoping/scripts.js
@@ -0,0 +1,15 @@
+const username = 'Tahsin'
+const userage = 22
+
+function add() {
+    const username = 'Ferdous'
+    const x = 5
+    const y = 8
+    console.log(x + y);
+    console.log(username);
+}
+add()
+console.log(username);
+
+
+console.log('Program ended');
