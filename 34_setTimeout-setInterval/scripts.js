ab831404c12bdf580e5e49f1146a238d5d3be000
tahsin-git-hub <tahsin@lexaeon.com>
2024-10-12 16:46:16 +0600

    setTimeout and setInterval in js

---
+++ 34_setTimeout-setInterval/scripts.js
@@ -0,0 +1,22 @@
+// setTimeout("console.log('Tahsin_Ferdous'); console.log('Ferdous_Tahsin');", 2000)
+// setTimeout(`console.log('Tahsin_Ferdous'); console.log('Ferdous_Tahsin');`, 2000)
+
+
+// const timer1 = setTimeout('console.log("Tahsin_Ferdous-1");', 1000)
+// const timer2 = setTimeout('console.log("Tahsin_Ferdous-2");', 2000)
+// const timer3 = setTimeout(a, 3000)
+
+
+// const timer1 = setInterval('console.log("Tahsin_Ferdous-1");', 1000)
+// const timer2 = setInterval('console.log("Tahsin_Ferdous-2");', 2000)
+const timer3 = setInterval(a, 1000)
+
+// clearTimeout(timer3)
+
+function a() {
+    console.log('Hello World!');
+}
+
+setTimeout(() => {
+    console.log('SUIII!');
+}, 2000);
