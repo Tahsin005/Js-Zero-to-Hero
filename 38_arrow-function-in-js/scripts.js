6022dcbc0f57908dad3bfcf241788fda1c3e05b6
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-02 03:13:52 +0600

    arrow function

---
+++ 38_arrow-function-in-js/scripts.js
@@ -0,0 +1,24 @@
+// // Function Declaration
+// function square(num) {
+//     return num * num;
+// }
+
+// // Function Expression
+// const square = function(num) {
+//     return num * num;
+// }
+
+// // Arrow Function Expression
+// const square = (num) => {
+//     return num * num;
+// }
+// const square = (num) => num * num;
+const square = num => num * num;
+
+
+setTimeout(() => {
+    console.log('Hi');
+}, 1000);
+
+
+const random = () => Math.random();
