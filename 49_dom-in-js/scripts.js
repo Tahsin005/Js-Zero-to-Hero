d93e271067d46e84cdabd27538facd680b317c63
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-11-10 23:11:43 +0600

    introduction to dom manipulation

---
+++ 49_dom-in-js/scripts.js
@@ -0,0 +1,3 @@
+function changeImage() {
+    document.body.children[4].src = 'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8='
+}
