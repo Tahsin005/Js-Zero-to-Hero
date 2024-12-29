55cf6e9759f4d73e3a34ce73c382c4572376b02d
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-12-29 10:19:54 +0600

    mouse event in js

---
+++ 62_mouse-events/scripts.js
@@ -0,0 +1,124 @@
+const h1 = document.querySelector('h1')
+const card = document.querySelector('.card')
+const container = document.querySelector('.container')
+
+let count = 0
+
+// card.addEventListener('click', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('dblclick', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('mousedown', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('mouseup', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('mouseenter', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('mouseleave', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('mousemove', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('mouseout', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('mouseover', () => { // works even when mouse enters the elements child.
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('wheel', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('touchstart', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('touchend', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('touchmove', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// h1.addEventListener('drag', () => { // Works only on desktop
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('pointermove', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+// card.addEventListener('pointerenter', () => {
+//     const newCard = document.createElement('div')
+//     newCard.classList.add('card')
+//     newCard.innerText = count++
+//     container.append(newCard)
+// })
+
+card.addEventListener('pointerleave', () => {
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
