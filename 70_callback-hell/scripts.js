39a70e5a2407a3f38e81b907518a1916b9b54529
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-06 16:24:31 +0600

    callback hell in js

---
+++ 70_callback-hell/scripts.js
@@ -0,0 +1,23 @@
+function makeHttpRequest(method, url, callback) {
+  const xhr = new XMLHttpRequest()
+  xhr.responseType = 'json'
+  xhr.addEventListener('load', () => {
+    callback(xhr.response)
+  })
+
+  xhr.open(method, url)
+  xhr.send()
+
+}
+
+makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
+  makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
+    console.log(postsData.posts[0].id)
+    makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
+      console.log(commentsData.comments[0].user.id)
+      makeHttpRequest('GET', `https://dummyjson.com/users/${usersData.users[0].id}`, (data) => {
+        console.log(data)
+      })
+    })
+  })
+})
