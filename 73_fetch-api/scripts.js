d92dab599cf65c6cbb3131e226f07d296ec0fb9c
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-06 20:40:13 +0600

    fetch api in js

--- 73_fetch-api/scripts.js
+++ 73_fetch-api/scripts.js
@@ -1,24 +1,46 @@
-fetch("https://dummyjson.com/products", {
-  method: "GET",
-})
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.error("Error:", error));
+// fetch("https://dummyjson.com/products", {
+//   method: "GET",
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => console.error("Error:", error));
 
 
 
-fetch("https://dummyjson.com/posts/add", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    title: "I am in love with Meem.",
-    userId: 5,
-  }),
-})
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.error("Error:", error));
+// fetch("https://dummyjson.com/posts/add", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({
+//     title: "I am in love with Meem.",
+//     userId: 5,
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => console.error("Error:", error));
+
+
+fetch('https://dummyjson.com/users')
+    .then((response) => response.json())
+    .then((usersData) => {
+        return fetch(`https://dummyjson.com/posts/user/${usersData.users[0].id}`);
+    })
+    .then((response) => response.json())
+    .then((postsData) => {
+        return fetch(`https://dummyjson.com/comments/post/${postsData.posts[0].id}`);
+    })
+    .then((response) => response.json())
+    .then((commentsData) => {
+        return fetch(`https://dummyjson.com/users/${commentsData.comments[0].user.id}`);
+    })
+    .then((response) => response.json())
+    .then((userData) => {
+        console.log("User data:", userData);
+    })
+    .catch((err) => {
+        console.error("Error:", err);
+    });
