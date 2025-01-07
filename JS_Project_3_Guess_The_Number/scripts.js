10a234802f456fb648b05e423fcefd78b6642f4b
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-07 22:29:13 +0600

    Guess the number project javascript

--- JS_Project_3_Guess_The_Number/scripts.js
+++ JS_Project_3_Guess_The_Number/scripts.js
@@ -6,27 +6,35 @@ const guessesSoFar = document.querySelector('#guesses-so-far');
 const guessCounter = document.querySelector('#guess-counter');
 let guesses = [];
 
-console.log(guessInput)
-console.log(guessInput.value)
 
 submitBtn.addEventListener('click', () => {
-    if (guessInput.value != '') {
+    if (guessInput.value != '' && message.innerText != `Congratulations! You guessed correctly!`) {
         guessesSoFar.innerText = '';
-        console.log(randomNumber, guessInput.value);
         let guessInputToNumber = parseInt(guessInput.value);
 
+        if (guessInputToNumber < 0 || guessInputToNumber > 100) {
+            message.classList.add('message-box')
+            message.innerText = `Enter a number between 0 to 100`;
+            return;
+        }
+
         if (guessInputToNumber < randomNumber) {
+            message.classList.add('message-box')
             message.innerText = `Your guess is low!`;
         } else if (guessInputToNumber > randomNumber) {
+            message.classList.add('message-box')
             message.innerText = `Your guess is high!`;
         } else {
+            message.classList.add('message-box')
             message.innerText = `Congratulations! You guessed correctly!`;
-            guessInput.value = '';
+
         }
         guesses.push(guessInput.value);
 
+
         for (let i = 0; i < guesses.length; i++) {
             guessesSoFar.innerText += `${guesses[i]}, `;
+            guessInput.value = '';
         }
         guessCounter.innerText = guesses.length;
     }
